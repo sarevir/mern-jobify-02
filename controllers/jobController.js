ff91c4dfced875a21c59382ae811c95161c25c14
sarevir0 <sarevir0@gmail.com>
2023-09-23 16:42:13 +0200

    9:76. Create Job Controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,3 +1,4 @@
+import Job from "../models/JobModel.js";
 import { nanoid } from "nanoid";
 
 let jobs = [
@@ -10,14 +11,8 @@ export const getAllJobs = async (req, res) => {
 };
 
 export const createJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    res.status(400).json({ msg: "please provide company and position" });
-    return;
-  }
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
+  const job = await Job.create(req.body);
+
   res.status(201).json({ job });
 };
 
