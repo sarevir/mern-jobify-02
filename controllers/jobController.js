9694750dc12369224c06c536ba331f0d466d95f2
sarevir0 <sarevir0@gmail.com>
2023-09-23 19:48:33 +0200

    9:82. More Custom Errors

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,6 @@
 import Job from "../models/JobModel.js";
 import { StatusCodes } from "http-status-codes";
+import { NotFoundError } from "../errors/customErrors.js";
 
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
@@ -15,7 +16,8 @@ export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
   if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
+    throw new NotFoundError(`no job with id ${id}`);
+    // return res.status(404).json({ msg: `no job with id ${id}` });
   }
   res.status(StatusCodes.OK).json({ job });
 };
@@ -24,7 +26,8 @@ export const updateJob = async (req, res) => {
   const { id } = req.params;
   const updatedJob = await Job.findByIdAndUpdate(id, req.body, { new: true });
   if (!updatedJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
+    throw new NotFoundError(`no job with id ${id}`);
+    // return res.status(404).json({ msg: `no job with id ${id}` });
   }
 
   res.status(StatusCodes.OK).json({ msg: `job modified`, job: updatedJob });
@@ -34,7 +37,8 @@ export const deleteJob = async (req, res) => {
   const { id } = req.params;
   const removedJob = await Job.findByIdAndDelete(id);
   if (!removedJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
+    throw new NotFoundError(`no job with id ${id}`);
+    // return res.status(404).json({ msg: `no job with id ${id}` });
   }
 
   res.status(StatusCodes.OK).json({ msg: `job deleted`, job: removedJob });
