834a06413088475644cd06e95b66e76421c713e3
sarevir0 <sarevir0@gmail.com>
2023-09-23 20:47:55 +0200

    9:85. Validation Middleware

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import mongoose from "mongoose";
-import { body, validationResult } from "express-validator";
-
+import { validateTest } from "./middleware/validationMiddleware.js";
 // routers
 import jobRouter from "./routes/jobRouter.js";
 
@@ -23,22 +22,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.post(
-  "/api/v1/test",
-  [body("name").notEmpty().withMessage("name is required")],
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const errorMessages = errors.array().map((error) => error.msg);
-      return res.status(400).json({ errors: errorMessages });
-    }
-    next();
-  },
-  (req, res) => {
-    const { name } = req.body;
-    res.json({ message: `hello ${name}` });
-  }
-);
+app.post("/api/v1/test", validateTest, (req, res) => {
+  const { name } = req.body;
+  res.json({ message: `hello ${name}` });
+});
 
 app.use("/api/v1/jobs", jobRouter);
 
