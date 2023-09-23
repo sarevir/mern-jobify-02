50a57b5031e75f8372cf358aff4f596e214d1620
sarevir0 <sarevir0@gmail.com>
2023-09-23 20:31:33 +0200

    9:84. Express Validator - Setup

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import mongoose from "mongoose";
+import { body, validationResult } from "express-validator";
 
 // routers
 import jobRouter from "./routes/jobRouter.js";
@@ -22,10 +23,22 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.post("/", (req, res) => {
-  console.log(req);
-  res.json({ message: "data received", data: req.body });
-});
+app.post(
+  "/api/v1/test",
+  [body("name").notEmpty().withMessage("name is required")],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      return res.status(400).json({ errors: errorMessages });
+    }
+    next();
+  },
+  (req, res) => {
+    const { name } = req.body;
+    res.json({ message: `hello ${name}` });
+  }
+);
 
 app.use("/api/v1/jobs", jobRouter);
 
